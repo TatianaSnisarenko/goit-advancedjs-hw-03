f335ecfd70c1e8eb18d2186e7f6b27e8a0932fce
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-10-04 20:20:41 +0300

    Add HomeWork 3

--- src/js/toast.js
+++ src/js/toast.js
@@ -26,3 +26,21 @@ export function showToast(type, message, title = '') {
     backgroundColor: color,
   });
 }
+
+export function showWarning(message) {
+  iziToast.warning({
+    message,
+    timeout: TOAST_TIMEOUT,
+    position: TOAST_POSITION,
+    close: false,
+  });
+}
+
+export function showError(message) {
+  iziToast.error({
+    message,
+    timeout: TOAST_TIMEOUT,
+    position: TOAST_POSITION,
+    close: false,
+  });
+}
